a1a75ef8822fefe6b1e42d5e0ff0e1529400af20
SergioManuelJob <suarezsergiomanuel69@gmail.com>
2023-11-27 20:39:48 +0000

    User updates his avatar succesfully

--- controllers/user.js
+++ controllers/user.js
@@ -42,8 +42,73 @@ function protected(req, res){
     res.status(200).send({msg: "Content of the endpoint that's protected"})
 }
 
+async function uploadAvatar(req, res){
+    const params = req.params
+
+    if(req.files){
+        const filePath =  req.files.avatar.path
+        const fileSplit = filePath.split("\\")
+        let fileName = fileSplit[1]
+        let extSplit = fileName.split(".")
+        let fileExt = extSplit[1]
+
+        if(fileExt !== "png" && fileExt !== "jpg")
+            res.status(400).send({msg: "The image extension is invalid. Only .jpg and .png is valid"})
+
+        let user = await User.findById({_id: params.id})
+        if(!user) res.status(404).send({ msg: "User could not be found"})
+        
+        user.avatar = fileName
+       
+        let userUpdated = await User.findByIdAndUpdate({ _id: params.id}, user)
+        console.log(userUpdated.email)
+        console.log(userUpdated.password)
+        console.log(userUpdated.avatar)
+        if(!userUpdated) res.status(400).send({msg: "The user could not be found"})
+        else res.status(200).send({msg: "Updated succesfully"})
+    }
+    
+
+    
+
+    // User.findById({_id: params.id}, (err, userData) => {
+
+    //         if(err) res.status(500).send({ msg: 'Server error'})
+
+    //         if(!userData) res.status(404).send({ msg: "User could not be found"})
+
+    //         let user = userData
+
+    //         if(req.files){
+    //             const filePath =  req.files.avatar.path
+    //             const fileSplit = filePath.split("/")
+    //             let fileName = fileSplit[1]
+    //             let extSplit = fileName.split(".")
+    //             let fileExt = extSplit[1]
+
+    //             if(fileExt !== "png" && fileExt !== "jpg")
+    //                 res.status(400).send({msg: "The image extension is invalid. Only .jpg and .png is valid"})
+    //             else {
+    //                 user.avatar = fileName
+    //                 User.findByIdAndUpdate({ _id: params.id}, user, (err, userResult) => {
+    //                     if(err) {
+    //                         res.status(500).send({msg: "Server error"})
+    //                     }
+    //                     if(!userResult) {
+    //                         res.status(404).send({ msg: "User could not be found"})
+    //                     }
+    //                     res.status(200).send({msg: "Updated succesfully"})
+    //                 })
+    //             }
+    //         }
+
+    //     }
+    // )
+}
+
 module.exports = {
     register,
     login,
-    protected
+    protected,
+    uploadAvatar
 }
--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,13 @@
 const express = require('express')
+const multipart = require('connect-multiparty')
 const UserController = require("../controllers/user")
 
 const md_auth = require('../middlewares/authenticated')
-
+const md_upload_avatar = multipart({ uploadDir: './uploads'})
 const api = express.Router()
 
 api.post('/register', UserController.register)
 api.post('/login', UserController.login)
 api.get("/protected",[md_auth.ensureAuth], UserController.protected)
+api.put('/upload-avatar/:id', [md_auth.ensureAuth, md_upload_avatar], UserController.uploadAvatar)
 module.exports = api
